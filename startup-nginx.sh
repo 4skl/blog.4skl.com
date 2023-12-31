50d5565fc9d2e6cf343232b686e9bfcd1005617d
4skl <medi.olivier@hotmail.fr>
2023-12-31 01:23:56 +0100

    Moved to relative path

--- startup-nginx.sh
+++ startup-nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Check nginx configuration, reload nginx, and obtain Let's Encrypt certificate if none present or renew it if needed, then stop nginx
-sh certificate-setup.sh
+sh ./certificate-setup.sh
 
 # Start supervisord
 supervisord -c /etc/supervisord.conf
