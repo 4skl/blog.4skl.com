22961f8f1bd03957860f8725ea3fadc30a7d1d7f
4skl <medi.olivier@hotmail.fr>
2024-01-02 06:23:50 +0100

    Improved certificate setup and renewal

--- certificate-setup.sh
+++ certificate-setup.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-# Check nginx configuration
-nginx -t
-
-# Reload nginx
-nginx -s reload
+# Check nginx configuration and reload it if it's valid
+nginx -t && nginx -s reload
 
 # Check if certificate exists
 if [ ! -d "/etc/letsencrypt/live/www.4skl.com" ]; then
@@ -12,10 +9,10 @@ if [ ! -d "/etc/letsencrypt/live/www.4skl.com" ]; then
 else
   # Ensure Nginx is configured to use the existing certificate and set up the redirection
   certbot --nginx --keep -d www.4skl.com --non-interactive --agree-tos --email $CERTBOT_EMAIL --redirect
-  # Renew Let's Encrypt certificate if due
-  certbot renew
+  # Renew Let's Encrypt certificate if due and reload Nginx if a certificate is actually renewed
+  certbot renew --deploy-hook "nginx -s reload"
   # Ensure Nginx is configured to use the certificate
-  certbot install --nginx -d www.4skl.com
+  certbot install --nginx --cert-name www.4skl.com --non-interactive
 fi
 # Stop nginx
 nginx -s stop
